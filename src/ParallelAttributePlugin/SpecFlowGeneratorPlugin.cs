2d8adc66446a08e09eb99b934c992bb087eee5ee
johnshaw1000 <johnshaw1000>
2015-06-12 20:54:48 +0100

    Initial

---
+++ src/ParallelAttributePlugin/SpecFlowGeneratorPlugin.cs
@@ -0,0 +1,23 @@
+﻿namespace ParallelAttributePlugin
+{
+    using BoDi;
+    using TechTalk.SpecFlow.Generator.Configuration;
+    using TechTalk.SpecFlow.Generator.Plugins;
+    using TechTalk.SpecFlow.Generator.UnitTestProvider;
+
+    public class SpecFlowGeneratorPlugin : IGeneratorPlugin
+    {
+        public void RegisterDependencies(ObjectContainer container)
+        {
+        }
+
+        public void RegisterCustomizations(ObjectContainer container, SpecFlowProjectConfiguration generatorConfiguration)
+        {
+            container.RegisterTypeAs<SpecFlowGeneratorProvider, IUnitTestGeneratorProvider>();
+        }
+
+        public void RegisterConfigurationDefaults(SpecFlowProjectConfiguration specFlowConfiguration)
+        {
+        }
+    }
+}
---
+++ src/ParallelAttributePlugin/SpecFlowGeneratorProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace ParallelAttributePlugin
+{
+    using System.CodeDom;
+    using TechTalk.SpecFlow.Generator;
+    using TechTalk.SpecFlow.Generator.UnitTestProvider;
+    using TechTalk.SpecFlow.Utils;
+
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class SpecFlowGeneratorProvider : NUnitTestGeneratorProvider
+    {
+        public SpecFlowGeneratorProvider(CodeDomHelper codeDomHelper) : base(codeDomHelper)
+        { }
+
+        public override void FinalizeTestClass(TestClassGenerationContext generationContext)
+        {
+            var parallelAttribute = new CodeAttributeDeclaration("NUnit.Framework.Parallelizable", new CodeAttributeArgument(new CodeTypeReferenceExpression("NUnit.Framework.ParallelScope.Fixtures")));
+
+            generationContext.TestClass.CustomAttributes.Add(parallelAttribute);
+            
+            base.FinalizeTestClass(generationContext);
+        }
+    }
+}
